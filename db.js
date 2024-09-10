f92c751a77eb51bf74dda36efa7f2bcb1e44e0d1
INTRUDER <dragula145@gmail.com>
2024-09-11 00:38:43 +0530

    adding for vercel

--- db.js
+++ db.js
@@ -8,7 +8,8 @@ let database;
 const getDatabase = async () => {
     const client = await mongoCl.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true);
+        useUnifiedTopology: true
+    });
     database = client.db('library');
 
     if (!database) {
