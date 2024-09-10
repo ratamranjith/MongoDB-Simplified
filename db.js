d25e6feb966c48230134924ff55a9715acec8bb0
INTRUDER <dragula145@gmail.com>
2024-09-11 01:39:29 +0530

    vercel fix

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mongodb = require('mongodb');
 const mongoCl = mongodb.MongoClient;
 const ObjectId = mongodb.ObjectId;
@@ -5,6 +6,7 @@ const ObjectId = mongodb.ObjectId;
 let database;
 
 const getDatabase = async () => {
+    console.log('MONGO_URI:', process.env.MONGO_URI);
     const uri = process.env.MONGO_URI;
     if (!uri) {
         console.error('MONGO_URI environment variable is not set.');
